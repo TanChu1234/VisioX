0c7208ff214a7776ce182d7fe489561a93ebbf80
TanChu1234 <chunhattan2001@gmail.com>
2026-03-30 02:01:40 +0700

    fix: Add generateStaticParams for static HTML export compatibility

--- app/datasets/[id]/annotate/[imageId]/page.tsx
+++ app/datasets/[id]/annotate/[imageId]/page.tsx
@@ -18,6 +18,19 @@ import {
 import AnnotationEditor from '@/components/annotate/AnnotationEditor';
 import BlueprintGrid from '@/components/BlueprintGrid';
 
+export async function generateStaticParams() {
+  const datasetIds = ['1', '2', '3', '4'];
+  const imageIds = Array.from({ length: 10 }, (_, i) => (i + 1).toString());
+  
+  const params = [];
+  for (const id of datasetIds) {
+    for (const imageId of imageIds) {
+      params.push({ id, imageId });
+    }
+  }
+  return params;
+}
+
 export default function AnnotatePage() {
   const params = useParams();
   const router = useRouter();
