13453c2947719909833cfb91758e0b7713f072a4
TanChu1234 <chunhattan2001@gmail.com>
2026-03-30 13:31:24 +0700

    chore: merge feature branch and finalize github actions deployment for main branch

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+const repo = isGithubActions ? process.env.GITHUB_REPOSITORY.split('/')[1] : "";
+const basePath = repo && !repo.endsWith('.github.io') ? `/${repo}` : "";
+
+const nextConfig = {
+  // Enables statically exporting the application into the `/out` directory required for GitHub Pages
+  output: "export",
+  // GitHub Pages cannot run the Next.js image optimization server, so we must output raw images
+  images: {
+    unoptimized: true,
+  },
+  // Automatically set the base path for GitHub Pages based on the repository name if in CI
+  basePath: basePath,
+};
+
+export default nextConfig;
